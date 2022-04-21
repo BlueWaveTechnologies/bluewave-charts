3baa9ffab51ca8cf9d60d524c2aef7bd5510c925
jbird32 <jbird3264@gmail.com>
2022-04-21 12:41:48 -0400

    Removed getStyleEditor from chart utils

--- src/Utils.js
+++ src/Utils.js
@@ -925,25 +925,6 @@ bluewave.chart.utils = {
 
 
   //**************************************************************************
-  //** getStyleEditor
-  //**************************************************************************
-    getStyleEditor : function(config){
-      //Create styleEditor as needed
-        if (!bluewave.charts.styleEditor){
-            bluewave.charts.styleEditor = new javaxt.dhtml.Window(document.body, {
-                title: "Edit Style",
-                width: 400,
-                valign: "top",
-                modal: false,
-                resizable: false,
-                style: config.style.window
-            });
-        }
-        return bluewave.charts.styleEditor;
-    },
-
-
-  //**************************************************************************
   //** createTooltip
   //**************************************************************************
     createTooltip: function(){
